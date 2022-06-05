00fc292f5aeb59d44bed6657af2688fde7492ea2
unknown <leonardocasinha12@gmail.com>
2022-06-05 20:17:03 -0300

    @Leonardo-SARTO-Conselheiro - [Task: Criando componente de Login] {05/06/2022 20:16}

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import './App.css';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login'
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{minHeight: '100vh'}}>
+      <div style={{minHeight: '130vh'}}>
         <Routes>
+
+          <Route path="/" element={<Login />} />
+
+          <Route path="/login" element={<Login />} />
 
           <Route path="/home" element={<Home />} />
 
